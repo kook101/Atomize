25e19be1aba07eaee6ed73c05c8951d4ae758fb3
kook101 <jaekook.h@gmail.com>
2020-10-21 23:14:56 -0700

    Atomizer_rotate_test

--- button3.js
+++ button3.js
@@ -38,64 +38,45 @@
 
 
 
-// console.log(domArr);
 
 for(let i=0; i<document.querySelectorAll('div').length; i++){
 
     var domArr = [];
     var domdomdom = document.querySelectorAll('div')[i];
     domdomdom.setAttribute('id', 'ato-'+i);
-    // domdomdom.setAttribute('class', 'ato');
-    // domdomdom.style.background = 'rgba(0, 0, 255, 0.1)'
+
 
     var domStyle = document.getElementById('ato-'+i);
     
 
-    // domStyle.addEventListener('mouseover', function(event){
-    //     event.target.style.color = 'orange';
-
-    // });
+    
 
 //outline
-    domStyle.style.border = 'solid 0.2px';
+    //domStyle.style.border = 'solid 0.1px';
     // domStyle.style.width = '50%';
     // domStyle.style.transformOrigin = 'center';
     // domStyle.style.backgroundColor = 'rgb(255,255,255)';
     
 //perspective and rotate should go together    
-    domStyle.style.perspective = '1000px';
-        // domStyle.style.transform = 'rotateX(60deg)'
-        domStyle.style.transform = 'rotateY(-30deg)';
+    domStyle.style.perspective = '10px';
+        // domStyle.style.transform = 'rotateX(60deg)';
+       
+        var degR = 0;
+        var id = setInterval(frame, 10);
+        function frame () {
+            if(degR == 100) {
+                clearInterval(id);
+            }else{
+                degR ++;  
+        
+       }
+       domStyle.style.transform = `rotateY(${degR}deg)`;
+    }
+       console.log(id);
+       // domStyle.style.transform = 
         // domStyle.style.transform = 'rotateZ(-45deg)'
-        domStyle.style.position = 'relative';
-
-//45
-    // domStyle.style.transform = 'rotateY(45deg)';
-
-//Isol..something
-
-
-// domStyle.style.zIndex = i;
-//     domStyle.style.paddingLeft = '100px'+ i ;
-
-    // domStyle.style.maxWidth = '1000px';
-    // domStyle.style.maxHeight = '600px;';
-    
-    
-
+        // domStyle.style.position = 'relative';
 
         domArr.push(domdomdom);
-        console.log(domArr);
-        
-
-           
-    };
-
-
-
-
-
-
-
-
-
+        // console.log(domArr);
+};
